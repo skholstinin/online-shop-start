774599f2f09422f70265c764e4499ff84d6a087a
sergey <s.kholstinin@yandex.ru>
2020-03-30 14:14:04 +0300

    Change database

--- src/main/java/com/example/WebMvcConfig.java
+++ src/main/java/com/example/WebMvcConfig.java
@@ -1,38 +1,61 @@
 package com.example;
 
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.view.InternalResourceView;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = {"com.example.api"})
 public class WebMvcConfig implements WebMvcConfigurer {
 
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//        registry.addResourceHandler("/**/*")
+//                .addResourceLocations("classpath:/static/")
+//                .resourceChain(true)
+//                .addResolver(new PathResourceResolver() {
+//                    @Override
+//                    protected Resource getResource(String resourcePath,
+//                                                   Resource location) throws IOException {
+//                        Resource requestedResource = location.createRelative(resourcePath);
+//                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+//                                : new ClassPathResource("/static/index.html");
+//                    }
+//                });
+//    }
+
+    @Value("${spring.resources.static-locations}")
+    String resourceLocations;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if (!registry.hasMappingForPattern("/**")) {
-            registry.addResourceHandler("/**")
-                    .addResourceLocations("classpath:/static/");
-        }
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index.html");
-    }
-
-    @Bean
-    public ViewResolver internalResourceViewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(InternalResourceView.class);
-        return viewResolver;
-    }
+//    @Bean
+//    ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+//        return new ErrorViewResolver() {
+//            @Override
+//            public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+//                return status == HttpStatus.NOT_FOUND
+//                        ? new ModelAndView("index.html", Collections.<String, Object>emptyMap(), HttpStatus.OK)
+//                        : null;
+//            }
+//        };
+//    }
 }
--- src/main/java/com/example/api/UserController.java
+++ src/main/java/com/example/api/UserController.java
@@ -35,6 +35,12 @@ public class UserController {
     AuthenticationManager authenticationManager;
 
 
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirect() {
+        // Forward to home page so that route is preserved.
+        return "forward:/index.html";
+    }
+
 //    @GetMapping(value = "/error")
 //    public String forwardTo() {
 //        return "forward:/index.html";
