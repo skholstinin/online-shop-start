7869f3100f621db25483301005fc324e6d9c8627
sergey <s.kholstinin@yandex.ru>
2020-04-02 11:58:24 +0300

    some changes

--- src/main/java/com/example/WebMvcConfig.java
+++ src/main/java/com/example/WebMvcConfig.java
@@ -2,11 +2,16 @@ package com.example;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
+@EnableWebMvc
+@ComponentScan("com.example")
 public class WebMvcConfig implements WebMvcConfigurer {
 
 //    @Value("${spring.resources.static-locations}")
@@ -22,9 +27,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
-                .addResourceLocations("classpath:/target/")
-                .addResourceLocations("classpath:/resources/static/**")
-                .addResourceLocations("classpath:/resources/");
+                .addResourceLocations("/", "/resources/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 
 }
