00a4b2f1789233faf861965212c277e31ff62c16
sergey <s.kholstinin@yandex.ru>
2020-04-02 12:16:59 +0300

    some changes

--- src/main/java/com/example/WebMvcConfig.java
+++ src/main/java/com/example/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.example;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,26 +12,26 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.example")
+@ServletComponentScan("com.example")
 public class WebMvcConfig implements WebMvcConfigurer {
 
 //    @Value("${spring.resources.static-locations}")
 //    String resourceLocations;
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry
-//                .addResourceHandler("/resources/**")
-//                .addResourceLocations("/resources/");
-//    }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/", "/resources/")
-                .setCachePeriod(3600)
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver());
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**")
+//                .addResourceLocations("/", "/resources/")
+//                .setCachePeriod(3600)
+//                .resourceChain(true)
+//                .addResolver(new PathResourceResolver());
+//    }
 
 }
